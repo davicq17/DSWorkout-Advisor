a598a6792a4a27fba2d7a4e0ee7b55f29738c76f
davicq17 <davidcarrillo1043@gamil.com>
2025-10-14 23:47:26 -0500

    tabla usuarios del admin

--- my_svelte/src/routes/confuser_ad/+page.svelte
+++ my_svelte/src/routes/confuser_ad/+page.svelte
@@ -1,40 +1,54 @@
-<script>
-  const Actualizar=()=>{
-  idUpt=localStorage.getItem("idUpdate")
-  nameI=document.getElementById('inputName').value;
-  surnameI=document.getElementById('inputLastName').value;
-  usernameI=document.getElementById('inputUsername').value;
-  emailI=document.getElementById('inputEmail').value;
-  passwordI=document.getElementById('inputPassword').value;
-  cellI=document.getElementById('inputCell').value;
-  rolI=document.getElementById('SelecRol').value;
-  axios({
-    method:'PUT',
-    url:'http://127.0.0.1:5000/editUser/'+idUpt,
-    data:{
-      id:idUpt,
-      username:usernameI,
-      name:nameI,
-      surname:surnameI,
-      email:emailI,
-      password:passwordI,
-      cell:cellI,
-      rol:rolI,
-    }
-  }).then(function(response){
-    alert(response.data.informacion)
+<script lang="ts">
+  import axios from "axios";
+  import {onMount} from 'svelte';
+  // almacen de datos
+  let usuarios:any[] =$state([]);
+  let userEdit:any = $state(null);
+  let userEliminar:any = $state(null);
 
-    document.getElementById("inputName").value = "";
-    document.getElementById("inputLastName").value = "";
-    document.getElementById("inputUsername").value = "";
-    document.getElementById("inputEmail").value = "";
-    document.getElementById("inputPassword").value = "";
-    document.getElementById("inputCell").value = "";
-    document.getElementById("SelecRol").value = 1;
-    localStorage.removeItem("idUpdate");
-    Init_Data()
-  }).catch(err => console.log('Error: ', err))
-}
+  onMount(()=>{
+    Init_Data();
+  });
+  // pedir datos de la bd
+  const Init_Data= async () =>{
+    try{
+      const response = await axios.get("http://127.0.0.1:5000/tableUser");
+      usuarios= response.data.map((u:any)=>({
+        ...u,
+        // combertidor del rol en texto
+        rolTxt:u.rol=== 1? "Administrador" : u.rol===2 ? "Usiario" : "Profesional",
+      }));
+      console.log("usuarios cargados:",usuarios);
+    }catch(err){
+      console.error("Error al cargar usuarios:",err);
+    }
+  };
+  // actualizar usuarios
+  const preEditar=(user:any)=>{userEdit={...user}}; 
+
+  const Actualizar= async()=>{
+    try{
+      await axios.put(`http://127.0.0.1:5000/edituser/${userEdit.id}`,userEdit);
+      alert("usuario actualizado correctamente");
+      userEdit=null;
+      await Init_Data();
+    }catch(err){
+      console.error("Error:",err);
+    }
+  };
+
+  // Función para eliminar usuario
+  const Eliminar= async ()=>{
+    try{
+      await axios.put(`http://127.0.0.1:5000/delete/${userEliminar.id}`);
+      alert("Usuario eliminado correctamente");
+      userEliminar = null;
+      await Init_Data();
+    }catch(err){
+      console.error("Error al eliminar:",err);
+    }
+  };
+
 </script>
 <div class="container my-5"><!--principal-->
   <div class="col-12  d-flex justify-content-center"><!--div1-->
@@ -55,6 +69,30 @@
       </tr>
     </thead>
     <tbody>
+      {#each usuarios as user }
+        <tr>
+          <td>{user.id}</td>
+          <td>{user.username}</td>
+          <td>{user.name}</td>
+          <td>{user.surname}</td>
+          <td>{user.email}</td>
+          <td>{user.password}</td>
+          <td>{user.cell}</td>
+          <td>{user.rolTxt}</td>
+          <td> 
+            <button onclick={()=>preEditar(user)} class="btn btn-warning btn-sm ms-3" data-bs-toggle="modal"data-bs-target="#Editar"aria-label="Editar usuario">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+              </svg>
+            </button>
+            <button onclick={()=>userEliminar=user} class="btn btn-warning btn-sm ms-3" data-bs-toggle="modal"data-bs-target="#Editar"aria-label="Editar usuario">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+              </svg>
+            </button>
+          </td>
+        </tr>
+      {/each}
     </tbody>
   </table>
       <!--Modal para confirmar la eliminacion de un usuario--> 
@@ -65,12 +103,11 @@
             <h1 class="text-center my-3 ms-4">Eliminar Usuario</h1>
           </div><!--div2.2.1-->
           <div class="modal-body"><!--div2.2.2-->
-          <p class="text-start ms-4">Seguro que deseas eliminar este usuario?</p>
+          <p class="text-start ms-4">¿Seguro que deseas eliminar al usuario <b>{userEliminar?.username}</b>? </p>
           </div><!--div2.2.2-->
           <div class="modal-footer d-flex justify-content-around"><!--div2.2.3-->
-            <a  class="btn btn-danger ms-5" data-bs-dismiss="modal" >Eliminar</a>
-            <!--onclick="Eliminar()"-->
-            <a class="btn btn-primary me-5 " data-bs-dismiss="modal">Cancelar</a>
+            <button onclick={Eliminar} class="btn btn-danger ms-5" data-bs-dismiss="modal" aria-label="eliminar usuario">Eliminar</button>
+            <button class="btn btn-primary" data-bs-dismiss="modal">Cancelar</button>
           </div><!--div2.2.3-->
         </div><!--div2.2-->
     </div><!--div2.1-->
@@ -82,43 +119,44 @@
           <div class="modal-header"><!--div5.1-->
             <h1 class="text-center my-3">Editar un registro</h1>
           </div><!--div5.1-->
-        <div class="modal-body"><!--div7-->
-          <div class="input-group"><!--div7.1-->
-          <div class="col-md-6 pe-2 mb-3">
-            <input type="text" class="form-control pe-2" id="inputName" placeholder="Nombres">
-          </div>
-          <div class="col-md-6 mb-3">
-              <input type="text" class="form-control" id="inputLastName" placeholder="Apellidos">
-          </div>
-        </div><!--div7.1-->
-        <div class="input-group"><!--div7.2-->
-          <div class="col-md-6 pe-2 mb-3">
-              <input type="text" class="form-control pe-2" id="inputUsername" placeholder="Usuario">
-          </div>
-          <div class="col-md-6 mb-3">
-              <input type="email" class="form-control" id="inputEmail" placeholder="Correo">
-          </div>
-        </div><!--div7.2-->
-        <div class="input-group"><!--div7.3-->
-          <div class="col-md-6 mb-3 pe-2">
-              <input type="text" class="form-control pe-2" id="inputPassword" placeholder="Ingrese su contraseña" maxlength="12" minlength="6">
-          </div>
-          <div class="col-md-6 mb-3">
-              <input type="number" class="form-control" id="inputCell" placeholder="Ingrese su contraseña" maxlength="10" minlength="10">
-          </div>
-        </div><!--div7.3-->
-        <div class="col-6"><!--div7.4-->
-          <select class="form-select"  id="SelecRol">
-            <option value="1">Administrador</option>
-            <option value="2">Usuario</option>
-            <option value="3">Profesional</option>
-        </select>
-        </div><!--div7.4-->
-        </div><!--div7-->
+          {#if userEdit}
+            <div class="modal-body"><!--div7-->
+              <div class="input-group"><!--div7.1-->
+                <div class="col-md-6 pe-2 mb-3">
+                  <input bind:value={userEdit.name} type="text" class="form-control pe-2" placeholder="Nombres">
+                </div>
+                <div class="col-md-6 mb-3">
+                  <input bind:value={userEdit.surname} type="text" class="form-control"  placeholder="Apellidos">
+                </div>
+              </div><!--div7.1-->
+              <div class="input-group"><!--div7.2-->
+                <div class="col-md-6 pe-2 mb-3">
+                  <input bind:value={userEdit.username} type="text" class="form-control pe-2"  placeholder="Usuario">
+                </div>
+                <div class="col-md-6 mb-3">
+                  <input bind:value={userEdit.email} type="email" class="form-control"  placeholder="Correo">
+                </div>
+              </div><!--div7.2-->
+              <div class="input-group"><!--div7.3-->
+                <div class="col-md-6 mb-3 pe-2">
+                  <input bind:value={userEdit.password} type="text" class="form-control pe-2" placeholder="Ingrese su contraseña" maxlength="12" minlength="6">
+                </div>
+                <div class="col-md-6 mb-3">
+                  <input bind:value={userEdit.cell} type="number" class="form-control"  placeholder="Ingrese su contraseña" maxlength="10" minlength="10">
+                </div>
+              </div><!--div7.3-->
+              <div class="col-6"><!--div7.4-->
+                <select bind:value={userEdit.rol} class="form-select" >
+                  <option value="1">Administrador</option>
+                  <option value="2">Usuario</option>
+                  <option value="3">Profesional</option>
+                </select>
+              </div><!--div7.4-->
+            </div><!--div7-->
+          {/if}
         <div class="modal-footer d-flex justify-content-around">
-          <a class="btn btn-primary ms-5" data-bs-dismiss="modal" >Actualizar</a>
-          <!--onclick="Actualizar()" -->
-          <a class="btn btn-danger me-5 " data-bs-dismiss="modal">Cancelar</a>
+          <button onclick={Actualizar} class="btn btn-primary ms-5" data-bs-dismiss="modal" >Actualizar</button>
+          <button class="btn btn-danger me-5 " data-bs-dismiss="modal">Cancelar</button>
         </div>
         </div><!--div5-->
       </div><!--div4-->
